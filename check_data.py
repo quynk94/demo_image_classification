df543fdfa54991e2714aad887ce19d7044127889
namnt <nguyentatnam53ac@gmail.com>
2019-07-06 09:39:58 +0700

    Update

--- check_data.py
+++ check_data.py
@@ -10,27 +10,33 @@ f = open(checkPathPass, 'r')
 checkContentPass = f.read()
 f.close()
 
+# print(checkContentPass)
+
 with open(filePath) as fp:
     line = fp.readline()
     cont = True
     while line and cont:
         line = fp.readline()
         if len(line) < 10:
-            next
-        imagePath, positions = line.split()
+            continue
+        data = line.split()
+        imagePath = data[0]
+        positions = data[1:]
         if imagePath in checkContentPass:
-            next
-        xMin, yMin, xMax, yMax, classValue = tuple(positions.split(','))
+            continue
         image = cv2.imread(imagePath)
-        cv2.rectangle(image, (int(xMin), int(yMin)),
-                      (int(xMax), int(yMax)), (0, 0, 255), 3)
+        for position in positions:
+            xMin, yMin, xMax, yMax, label = tuple(position.split(','))
+            cv2.rectangle(image, (int(xMin), int(yMin)),
+                          (int(xMax), int(yMax)), (0, 0, 255), 3)
+            cv2.putText(image, label, (int(xMin), int(yMin) - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
         cv2.imshow('Rec', image)
         while True:
             k = cv2.waitKey(1000) & 0xFF
             if k == ord('s'):
                 print(imagePath)
                 cv2.destroyAllWindows()
-                cont = False
                 f = open(checkPathFail, 'a')
                 f.write("%s\n" % (imagePath))
                 f.close()
--- preprocess.py
+++ preprocess.py
@@ -7,9 +7,12 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("-d", "--dataset", required=True,
                     help="path to input dataset")
+    ap.add_argument("-r", "--resize", default='1',
+                    help="need resize or not")
     args = vars(ap.parse_args())
 
-    resize_image.process(args['dataset'])
+    if args['resize'] == '1':
+        resize_image.process(args['dataset'])
     crop_and_label_images.process(args['dataset'])
 
 
